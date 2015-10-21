497d3fd0318e32e1c6502d38ad147c2b0b5b97a8
noobandy <noobandy1364@gmail.com>
2015-10-21 14:58:13 +0530

    integrated jade templating engine

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 var path = require("path");
 var express = require("express");
+var jade = require("jade");
+
 var index = require(path.join(__dirname, "./routes/index.js"));
 var pages = require(path.join(__dirname, "./routes/pages.js"));
 
 var app = new express();
 
+app.engine("jade", function(filePath, options, callback) {
+	return callback(null,jade.renderFile(filePath, options));
+});
+
+app.set("views", path.join(__dirname, "./views"));
+
+app.set("view engine", "jade");
+
 //static files 
-
+//public
 app.use("/public", express.static(path.join(__dirname,"public")));
-
+//bower components
 app.use("/bower_components", express.static(path.join(__dirname,"bower_components")));
+
 
 app.use("", index);
 
 app.use("/pages", pages);
 
+
 var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('ToDo app listening at http://%s:%s', host, port);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "../views/index.html"));
+router.get("/", function(req, res, next) {
+	res.render("index");
 });
 
 
