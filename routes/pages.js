e748b5465d835d774da3eb9895dc9c4f25c7903b
noobandy <noobandy1364@gmail.com>
2015-10-29 23:17:47 +0530

    Jquer Plugin

--- routes/pages.js
+++ routes/pages.js
@@ -2,21 +2,8 @@ var path = require("path");
 var express = require("express");
 var router = express.Router();
 
-router.get("/agenda", function(req, res) {
-	res.sendFile(path.join(__dirname, "../views/pages/agenda.html"));
+router.get("/:page", function(req, res) {
+	res.sendFile(path.join(__dirname, "../views/pages/"+req.params.page+".html"));
 });
-
-router.get("/clock", function(req, res) {
-	res.sendFile(path.join(__dirname, "../views/pages/clock.html"));
-});
-
-router.get("/hospital", function(req, res) {
-	res.sendFile(path.join(__dirname, "../views/pages/hospital.html"));
-});
-
-router.get("/scratchpad", function(req, res) {
-	res.sendFile(path.join(__dirname, "../views/pages/scratchpad.html"));
-});
-
 
 module.exports = router;
